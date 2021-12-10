ad9ad47ecf301e4df6e57b77fcb5c8c951f00b47
Sriram <invadersriram2002@gmail.com>
2021-12-10 23:20:20 +0530

    add mape list

--- main.py
+++ main.py
@@ -1,4 +1,39 @@
 import streamlit as st
 import pandas as pd
 
-st.write("# Hi there")
+st.write("# Effect of train data size")
+
+@st.cache
+def get_data():
+    df = pd.read_csv("fish.csv")
+    return df
+df = get_data()
+
+@st.cache
+def show_data_func():
+    df_to_display = df.iloc[:5]
+    return df_to_display
+
+show_data = st.checkbox("Show data")
+if show_data:
+    st.table(show_data_func())
+
+x = df[["Height", "Width", "Length"]]
+y = df["Weight"]
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_percentage_error
+
+mape_lst = []
+
+for train_size in range(1, 10):
+    train_size = train_size / 10
+    train_x, test_x, train_y, test_y = train_test_split(x, y, train_size=train_size)
+    m = LinearRegression()
+    m.fit(train_x, train_y)
+    p = m.predict(test_x)
+    mape = mean_absolute_percentage_error(p, test_y)
+    mape_lst.append(mape)
+
+st.write(mape_lst)
