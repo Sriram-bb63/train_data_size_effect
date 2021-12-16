c830b9f946564548f2fd1e5eb6bef8b5ae4e1b3e
Sriram <invadersriram2002@gmail.com>
2021-12-16 23:34:04 +0530

    better altair plotting method

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ st.write("---")
 st.write("## Choose models")
 
 decision_tree_checkbox = st.checkbox("Decision tree")
-random_forest_checkbox = st.checkbox("Ridge")
+ridge_checkbox = st.checkbox("Ridge")
 
 from sklearn.linear_model import LinearRegression
 
@@ -37,7 +37,7 @@ models = {"Linear regression": LinearRegression()}
 if decision_tree_checkbox:
     from sklearn.tree import DecisionTreeRegressor
     models["Decision tree"] = DecisionTreeRegressor()
-if random_forest_checkbox:
+if ridge_checkbox:
     from sklearn.linear_model import Ridge
     models["Ridge"] = Ridge()
 
@@ -66,7 +66,7 @@ mape_df["Train data size"] = [i for i in range(10, 91, 10)]
 mape_df["Linear regression"] = mape_dict["Linear regression"]
 if decision_tree_checkbox:
     mape_df["Decision tree"] = mape_dict["Decision tree"]
-if random_forest_checkbox:
+if ridge_checkbox:
     mape_df["Ridge"] = mape_dict["Ridge"]
 
 st.write("---")
@@ -80,8 +80,8 @@ chart_style = st.selectbox("Chart style", ("Matplotlib", "Vega"))
 
 if chart_style == "Matplotlib":
     mape_fig, ax = plt.subplots()
-    for col in mape_df.columns:
-        ax.plot(mape_df.index*10, mape_df[col], marker=".", label=col)
+    for col in mape_df.iloc[:, 1:].columns:
+        ax.plot(mape_df["Train data size"], mape_df[col], marker=".", label=col)
     ax.legend(loc="upper right")
     ax.set_xlabel("Train data size in %")
     ax.set_ylabel("MAPE")
@@ -92,7 +92,7 @@ if chart_style == "Matplotlib":
     if average_checkbox:
         avg_fig, ax = plt.subplots()
         avg_mape_dict = dict()
-        for col in mape_df.columns:
+        for col in mape_df.iloc[:, 1:].columns:
             avg_mape = sum(mape_df[col]) / len(mape_df[col])
             avg_mape_dict[col] = avg_mape
         ax.bar(avg_mape_dict.keys(), avg_mape_dict.values())
@@ -103,77 +103,36 @@ elif chart_style == "Vega":
         x=alt.X('Train data size',
                 axis=alt.Axis(title='Train size in %')
             ),
-            y=alt.Y("Linear regression",
-                    axis=alt.Axis(title='MAPE'),
-                    scale=alt.Scale(domain=(0, 1))
-            )
-        )
-    for col in mape_df.iloc[:, 2:]:
-        mape_chart_temp = alt.Chart(mape_df).mark_line(point=alt.OverlayMarkDef()).encode(
+        y=alt.Y("Linear regression",
+                axis=alt.Axis(title='MAPE'),
+                scale=alt.Scale(domain=(0, 1))
+            ),
+        color=alt.value("#5072aa")
+    )
+    if decision_tree_checkbox:
+        mape_chart += alt.Chart(mape_df).mark_line(point=alt.OverlayMarkDef()).encode(
             x=alt.X('Train data size',
                     axis=alt.Axis(title='Train size in %')
                 ),
-            y=alt.Y(col,
+            y=alt.Y("Decision tree",
                     axis=alt.Axis(title='MAPE'),
                     scale=alt.Scale(domain=(0, 1))
-                )
-            )
-        mape_chart = mape_chart + mape_chart_temp
+                ),
+            color=alt.value("#57a76a")
+        )
+    if ridge_checkbox:
+        mape_chart += alt.Chart(mape_df).mark_line(point=alt.OverlayMarkDef()).encode(
+            x=alt.X('Train data size',
+                    axis=alt.Axis(title='Train size in %')
+                ),
+            y=alt.Y("Ridge",
+                    axis=alt.Axis(title='MAPE'),
+                    scale=alt.Scale(domain=(0, 1))
+                ),
+            color=alt.value("#bd555a")
+        )
     st.altair_chart(mape_chart, use_container_width=True)
-    
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# if chart_style == "Matplotlib":
-#     plt.rcParams["figure.figsize"] = (10,5)
-#     if bar:
-#         plt.bar(mape_df["train_size"], mape_df["mape"], width=7)
-#         plt.plot([5, 95], [avg, avg], color="red")
-#         plt.ylim([0, 1])
-#         plt.legend(loc="upper right")
-#         plt.xlabel("Train size in %")
-#         plt.ylabel("MAPE")
-#         plt.grid(axis="y")
-#         plt.tight_layout()
-# elif chart_style == "Vega":
-#     if bar:
-#         mape_chart = alt.Chart(mape_df).mark_bar(size=50).encode(
-#             x=alt.X('train_size',
-#                 axis=alt.Axis(title='Train size in %')
-#                 ),
-#             y=alt.Y('mape',
-#                 axis=alt.Axis(title='MAPE'),
-#                 scale=alt.Scale(domain=(0, 1))
-#                 )
-#             )
-#         line = alt.Chart(avg_df).mark_rule(color="red").encode(y='y')
-#         st.altair_chart(mape_chart + line, use_container_width=True)
-#     else:
-#         mape_chart = alt.Chart(mape_df).mark_line(point=alt.OverlayMarkDef()).encode(
-#         x=alt.X('train_size',
-#                 axis=alt.Axis(title='Train size in %')
-#                 ),
-#         y=alt.Y('mape',
-#                 axis=alt.Axis(title='MAPE'),
-#                 scale=alt.Scale(domain=(0, 1))
-#                 )
-#             )
-#         line = alt.Chart(avg_df).mark_rule(color="red").encode(y='y')
-#         st.altair_chart(mape_chart + line, use_container_width=True)
 
 smol_docs = """
 ## What is train size?
@@ -197,25 +156,3 @@ Source code [Github](https://github.com/Sriram-bb63/train_data_size_effect)
 """
 
 st.sidebar.write(links)
-
-
-
-
-
-
-
-
-
-
-
-# def trainer(models, mape_lst):
-#     for model in models:
-#         model_mape_lst = []
-#         for train_size in range(10, 91, 10):
-#             train_x, test_x, train_y, test_y = train_test_split(x, y, random_state=1, train_size=train_size)
-#             model.fit(train_x, train_y)
-#             p = model.predict(test_x)
-#             mape = mean_absolute_percentage_error(p, test_y)
-#             model_mape_lst.append(mape)
-#         mape_lst.append(model_mape_lst)
-#     return mape_lst
